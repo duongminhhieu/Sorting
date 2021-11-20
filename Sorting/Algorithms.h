370ed9739a5db0e9a38846aeafe517f248e59de2
= <=>
2021-11-20 21:17:41 +0700

    ..

--- Sorting/Algorithms.h
+++ Sorting/Algorithms.h
@@ -11,7 +11,12 @@ void selectionSort(int arr[], unsigned int n);
 void selectionSort_countComp(int arr[], int n, unsigned long int& count_comp);
 void heapify(int* arr, int n, int i);
 void heapSort(int* arr, int n);
+void heapSort_CountComp(int* arr, int n, unsigned long long int& count_comp);
+void heapify_CountComp(int* arr, int n, int i, unsigned long long int& count_comp);
 void countingSort(int arr[], int n);
+void countingSort_CountComp(int arr[], int n, unsigned long long int& count_comp);
+
+
 
 void bubbleSort(int A[], int n);
 int partition(int A[], int first, int last);

4bef22f7a4494ada08cb85082d191f6462bc8dd4
Hieu <bihisu02@gmail.com>
2021-11-22 01:46:41 +0700

    Add Command 3

--- Sorting/Command.cpp
+++ Sorting/Command.cpp
@@ -7,6 +7,7 @@ string Algorithm_name[10] = { "selection-sort","insertion-sort","bubble sort","h
 string Input_order[4] = { "-rand","-sorted","-rev" ,"-nsorted" };
 string Input_order2[4] = { "randomize","sorted","reverse sorted"," nearly sorted" };
 string Output_parameters[3] = { "-time","-comp","-both" };
+string Data_Order[4] = { "Randomize", "Sorted", "Reversed", "Nearly Sorted" }; //command 3
 
 bool isNumber(string s)
 {
@@ -423,4 +424,249 @@ bool Command_2(char* argv[])
 	}
 	writeArrayToFile("output.txt", a, n);
 
+}
+
+bool Command_3(char* argv[])
+{
+	int algorithm = isAlgorithm_name(argv[2]);
+	if (algorithm == -1)
+	{
+		cout << "Lenh khong hop le" << endl;
+		return false;
+	}
+
+	int size;
+	if (isNumber(argv[3]))
+	{
+		size = atoi(argv[3]); //ham co san chuyen string sang int
+	}
+	else
+	{
+		cout << "Lenh khong hop le" << endl;
+		return false;
+	}
+
+	int output = isOutput_parameters(argv[4]);
+	if (output == -1)
+	{
+		cout << "Lenh khong hop le" << endl;
+		return false;
+	}
+
+	cout << "ALGORITHM MODE" << endl;
+	cout << "Algorithm: " << Algorithm_name[algorithm] << endl;
+	cout << "Input size: " << size << endl << endl;
+
+	//chay Algorithm cho 4 kieu Data Order voi Data Size = size cho truoc
+	int* a = new int[size];
+	double runtime[4];
+	unsigned long long int count_comp[4];
+	clock_t start, end;
+	switch (algorithm)
+	{
+	case 0:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			selectionSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			selectionSort_countComp(a, size, count_comp[i]);
+		}
+		break;
+
+	case 1:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			insertionSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			insertionSort_countComp(a, size, count_comp[i]);
+		}
+		break;
+
+	case 2:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			bubbleSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			bubbleSortCountcomp(a, size, count_comp[i]);
+		}
+		break;
+
+	case 3:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			heapSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			heapSort_CountComp(a, size, count_comp[i]);
+		}
+		break;
+
+	case 4:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			mergeSort(a, 0, size - 1);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			mergeSort_countComp(a, 0, size - 1, count_comp[i]);
+		}
+		break;
+
+	case 5:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			quickSort(a, 0, size - 1);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			quickSortCountcomp(a, 0, size - 1, count_comp[i]);
+		}
+		break;
+
+	case 6:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			radixSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			radixSort_comp(a, size, count_comp[i]);
+		}
+		break;
+
+	case 7:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			shakerSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			shakerSortCountcomp(a, size, count_comp[i]);
+		}
+		break;
+
+	case 8:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			shellSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			shellSort_countComp(a, size, count_comp[i]);
+		}
+		break;
+
+	case 9:
+		for (int i = 0; i < 4; i++)
+		{
+			//tao Data Order voi Data Size = size
+			GenerateData(a, size, i);
+
+			//tinh Run Time va Comparision cua Algorithm
+			start = clock();
+			countingSort(a, size);
+			end = clock();
+			runtime[i] = (double)(end - start) / CLOCKS_PER_SEC;
+			runtime[i] *= 1000;
+
+			count_comp[i] = 0;
+			countingSort_CountComp(a, size, count_comp[i]);
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "Input order: " << Data_Order[i] << endl
+			<< "-------------------------------" << endl;
+		switch (output)
+		{
+		case 0:
+			cout << "Running time: " << runtime[i] << " ms" << endl;
+			break;
+		case 1:
+			cout << "Comparisons: " << count_comp[i] << endl;
+			break;
+		case 2:
+			cout << "Running time: " << runtime[i] << " ms" << endl;
+			cout << "Comparisons: " << count_comp[i] << endl;
+			break;
+		default:
+			break;
+		}
+		cout << endl;
+	}
 }
